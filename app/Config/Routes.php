aa17e8965c60f49e69a115be11f52fbd2cc72c91
Arthur Campelo <arthurj345@gmail.com>
2026-08-16 21:28:59 -0300

    ROUTER: Setadas as rotas base protegidas pelo shield + o alias de session

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,11 @@ use CodeIgniter\Router\RouteCollection;
 /** @var RouteCollection $routes */
 $routes->get('/', 'Home::index');
 
+// Authenticação de rotas automática pelo shield
 service('auth')->routes($routes);
+
+$routes->group('', ['filter' => 'session'], static function ($routes) {
+    $routes->presenter('vehicles', ['controller' => 'VehicleController']);
+    $routes->presenter('drivers', ['controller' => 'DriverController']);
+    $routes->presenter('trips', ['controller' => 'TripController']);
+});
